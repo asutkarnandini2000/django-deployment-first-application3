16f51fa33c4f0256af08e5a4ae439f37ef8dceca
asutkarnandini2000 <121220930+asutkarnandini2000@users.noreply.github.com>
2022-12-22 14:51:08 +0530

    Update views.py

--- FirstProject/FirstApp/views.py
+++ FirstProject/FirstApp/views.py
@@ -179,3 +179,8 @@ def homepage(request):
         <h3>Plz try other URL or Links!!!</h3>
     </center>''';
     return HttpResponse(htmldata);    
+
+
+#git_view_func()
+def git_view_func(request):
+	return HttpResponse("<h1>Hello from git hub view function</h1><hr  />");
